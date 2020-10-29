bea432ae4427dd51bef6a62f27f20e7d0c9922a4
Md-Moniruzzaman <ammoniruzzaman@gmail.com>
2020-10-29 22:08:16 +0600

    string method

--- Project-1.py
+++ Project-1.py
@@ -10,8 +10,18 @@
 # print(course)
 
 # formated string
-first_name = "Md"
-last_name = "Moniruzzaman"
-message = first_name+' ' + last_name + ' is a coder.'
-msg = f'{first_name} [{last_name}] is a coder.'
-print(msg)
+# first_name = "Md"
+# last_name = "Moniruzzaman"
+# message = first_name+' ' + last_name + ' is a coder.'
+# msg = f'{first_name} [{last_name}] is a coder.'
+# print(msg)
+
+# string method
+
+course = 'Python for Begginers'
+
+print(len(course))
+print(course.upper())
+print(course.find("f"))
+print(course.replace('Begginers','Beginners'))
+print(course.title())
